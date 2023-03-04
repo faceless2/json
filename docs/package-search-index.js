92baf7fa851b974039b2c2d7f47c1a4e1a4f1f6d
Mike <mike@qtunes.org>
2023-03-04 11:05:13 +0000

    Rebuilt gh-pages

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.bfo.json"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.bfo.box"},{"l":"com.bfo.json"}];updateSearchResults();
