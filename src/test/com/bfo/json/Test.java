efdc7720be0e1c41bbd21362ad4341440d8bc635
Mike <mike@qtunes.org>
2024-11-20 21:43:22 +0000

    Added Yaml testcases

--- src/test/com/bfo/json/Test.java
+++ src/test/com/bfo/json/Test.java
@@ -19,6 +19,8 @@ public class Test {
         System.out.flush();
         TestPartial.main(args);
         System.out.flush();
+        TestYaml.main(args);
+        System.out.flush();
         Speed.main(args);
         System.out.flush();
     }
